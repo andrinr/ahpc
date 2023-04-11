be582c0c5d5eef9297075536a66748ddf6adaf8b
andrinr <andrinrehmann@gmail.com>
2023-04-11 18:28:44 +0200

    new structure

--- exercise01/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "blitz/array.h"
-
-int  main() {
-    blitz::Array <float ,3> data (10,8,6);
-
-    std::cout  << data << std::endl;
-}
--- exercise02/task4/plot.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-data = np.loadtxt('out.txt', delimiter=' ', unpack=True)
-data = np.log(data)
-plt.imshow(data, cmap='plasma', interpolation='nearest')
-plt.show()
